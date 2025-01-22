67d4fe675d532c6c9e8cdb63bfc04158aeaab2ec
heon-kim <98210662+heon-kim@users.noreply.github.com>
2025-01-22 13:20:12 +0900

    👔 [add] 상품 가입 모달 추가

--- src/pages/ProductPage/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductPage/ProductDetail/ProductDetail.tsx
@@ -1,13 +1,29 @@
 import * as styled from "../styles"
-import Header from "../../../layout/Header"
 import ReactApexChart from "react-apexcharts"
 import { useChartData } from "../hooks/useChartData";
 // import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+
+
+// components
+import Header from "../../../layout/Header"
 import BlueButton from "../../../ui/BlueBtn";
+import Modal from "../../../layout/Modal";
 
 function ProductDetail() {
 	// const { id } = useParams();
 	const { chartData } = useChartData();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	}
 
 	return (
 		<styled.Container>
@@ -30,22 +46,44 @@ function ProductDetail() {
 
 			<h3>수익률 추이</h3>
 			<ReactApexChart options={chartData.options} series={chartData.series} type="area" height={350} />
-			
+
 			<h3>수수료 추이</h3>
 			<ReactApexChart options={chartData.options} series={chartData.series} type="area" height={350} />
 
 
 			<h3>관련 상품</h3>
-				<styled.GridContainer>
-					{["상품1", "상품2"].map((name, index) => (
-						<styled.GridItem key={index}>
-							{name}
-						</styled.GridItem>
-					))}
-                </styled.GridContainer>
-			<BlueButton>가입하기</BlueButton>
+			<styled.GridContainer>
+				{["상품1", "상품2"].map((name, index) => (
+					<styled.GridItem key={index}>
+						{name}
+					</styled.GridItem>
+				))}
+			</styled.GridContainer>
+			<styled.ButtonContainer>
+				<BlueButton onClick={() => openModal()}>
+					상품 가입하기
+				</BlueButton>
+			</styled.ButtonContainer>
+
+			<Modal isOpen={isModalOpen} onClose={closeModal}>
+				<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '30px', textAlign: 'center', fontSize: '18px' }}>
+
+					<div>
+						<DotLottieReact
+							src="https://lottie.host/4f4850d1-a0b3-4887-8615-108bba756fd0/NUT4PqgDtr.json"
+							loop
+							autoplay
+						/>
+						<p>2천 만원으로 3년 가입 시<br />150만원의 수익을 예상해요.</p>
+					</div>
+					<p style={{fontWeight: '600'}}>3년 후에는 1년에 7만원,<br />한달에 5천원 더 사용할 수 있어요.</p>
+					<BlueButton>가입하기</BlueButton>
+				</div>
+			</Modal>
 		</styled.Container>
 	);
 }
 
 export default ProductDetail;
+
+
