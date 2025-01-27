06a0c8701ce481665adbfc39ebcab27eeeef52b0
htchoi1006 <htchoi1006@kookmin.ac.kr>
2025-01-27 20:58:22 +0900

    ✨ [feat] 로그아웃, 회원 탈퇴 기능 구현 및 회원가입 후 로그인 페이지로 라우팅

--- src/services/api/Member.ts
+++ src/services/api/Member.ts
@@ -1,28 +1,37 @@
-import { request } from '../request';
-import { config } from '../../config/config';
-import { MemberConnectedResponseDTO, MemberWishResponseDTO, MemberInfoResponseDTO } from '../dto/Member';
+import { request } from "../request";
+import { config } from "../../config/config";
+import {
+	MemberConnectedResponseDTO,
+	MemberWishResponseDTO,
+	MemberInfoResponseDTO,
+} from "../dto/Member";
 
 const BASE_URL = config.apiUrl;
 
 export const memberService = {
-    getMemberConnected: () => {
-        return request<MemberConnectedResponseDTO>({
-            method: 'GET',
-            url: `${BASE_URL}/member/connected`,
-        });
-    },
-    getMemberWish: () => {
-        return request<MemberWishResponseDTO>({
-            method: 'GET',
-            url: `${BASE_URL}/member/wish`,
-        })
-    },
-    getMemberInfo: (
-		config?: { headers: { Authorization: string } }
-	) => {
+	getMemberConnected: () => {
+		return request<MemberConnectedResponseDTO>({
+			method: "GET",
+			url: `${BASE_URL}/member/connected`,
+		});
+	},
+	getMemberWish: () => {
+		return request<MemberWishResponseDTO>({
+			method: "GET",
+			url: `${BASE_URL}/member/wish`,
+		});
+	},
+	getMemberInfo: (config?: { headers: { Authorization: string } }) => {
 		return request<MemberInfoResponseDTO>({
 			method: "GET",
 			url: `member/me`,
+			headers: config?.headers,
+		});
+	},
+	deleteAccount: (config?: { headers: { Authorization: string } }) => {
+		return request<void>({
+			method: "POST",
+			url: `/auth/unsubscribe`,
 			headers: config?.headers,
 		});
 	},
